f6ce3dbe591e25012a2ec29389df1a0305e8d7f2
Dharamveer Bhadoria <singhbhadoria96@yahoo.in>
2023-10-07 12:28:42 +0000

    week2 live stream continued...

--- variables.tf
+++ variables.tf
@@ -6,10 +6,10 @@ variable "bucket_name" {
     type = string
 }
 
-variable "index_html_path" {
-    type = string
-}
+# variable "index_html_path" {
+#     type = string
+# }
 
-variable "error_html_path" {
-    type = string
-}
+# variable "error_html_path" {
+#     type = string
+# }
